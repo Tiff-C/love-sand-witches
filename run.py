dabda77de655048eb889ebbda2f78719dbe92855
Tiff-C <deguntiffany@gmail.com>
2021-09-06 16:39:05 +0000

    collect sales data from last 5 markets in lists of colums

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import gspread
 from google.oauth2.service_account import Credentials
+from pprint import pprint
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -81,6 +82,21 @@ def calculate_surplus_data(sales_row):
     return surplus_data
 
 
+def get_last_5_entries_sales():
+    """
+    Collects columns of data from sales worksheet, collecting
+    the last 5 entries for each sandwich and returns the data
+    as a list of lists.
+    """
+    sales = SHEET.worksheet('sales')
+    
+    columns = []
+    for ind in range(1, 7):
+        column = sales.col_values(ind)
+        columns.append(column[-5:])
+    pprint(columns)
+
+
 def main():
     """
     Run all program functions
@@ -93,4 +109,5 @@ def main():
 
 
 print('Welcome to Love Sand Witches Data Automation!\n')
-main()
+# main()
+get_last_5_entries_sales()
